13194d2a4e0ca8c4a35ad2cd905e24f093839101
MinhVM(MinhPC) <minhsiudepzai2k3@gmail.com>
2024-07-05 21:31:56 +0700

    Create not.is-an.app.js

---
+++ domains/not.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'A website for my vercel bot',
+  domain: 'is-an.app',
+  subdomain: 'not',
+  owner: {
+    email: 'minhvmware@duck.com',
+  },
+  record: {
+    CNAME: 'minhvmware.top',
+    TXT: ['dh=4d2afead8a4b28129541bbf95b156eeaa6b2632b'],
+  },
+  proxy: false
+})
