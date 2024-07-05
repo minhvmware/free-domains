aafdba5f3e109b15a9cb614c83fc062a6d6e925a
MinhVM(MinhPC) <minhsiudepzai2k3@gmail.com>
2024-07-05 21:29:11 +0700

    Update not.is-an.app.js

--- domains/not.is-an.app.js
+++ domains/not.is-an.app.js
@@ -7,7 +7,7 @@ addSubDomain({
   },
   record: {
     CNAME: 'minhvmware.top',
-    TXT: ['_discord.not.is-an.app', 'txt', 'dh=4d2afead8a4b28129541bbf95b156eeaa6b2632b'],
+    TXT: ['dh=4d2afead8a4b28129541bbf95b156eeaa6b2632b'],
   },
   proxy: false
 })
